780fe9480da2263eacc53ab5ef899cef07170b9e
Michelle Sharpe <hidibyms@gmail.com>
2019-10-07 19:22:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,8 +39,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(num) {
   var array = [];
   do {
-    // array.push('I love the Beatles!');
-    num++;
+     num++;
   } while (num < 8);{
     array.push('I love the Beatles!');
   }
