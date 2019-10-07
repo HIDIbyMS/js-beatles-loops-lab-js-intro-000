153f1f8ce4eca8904e36cee752770df8fb119f14
Michelle Sharpe <hidibyms@gmail.com>
2019-10-07 18:41:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // add solution here
+
+var empty = [];
+
+// for loop
+function theBeatlesPlay(musicians, instruments) {
+ for (let i = 0; i < musicians.length; i++){
+   empty.push(musicians[i] + " plays " + instruments[i]);
+ }
+  return empty;
+}
+
+
+
+// while loop
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i < facts.length) {
+    i++;
+   console.log(facts[i] +"!!!");
+  } 
+}
+
+
+
+
+// do while loop
+function iLoveTheBeatles(num) {
+  var array = [];
+  var i = 0;
+  do {
+    array.push("I love the Beatles!");
+    i++;
+  } while (i < 8 || num == 17);
+  return array;
+}
